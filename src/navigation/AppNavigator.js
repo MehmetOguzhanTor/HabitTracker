e1b285b9567d2d046746160313c362b5593580ee
MehmetOguzhanTor <mehmetoguzhantor@gmail.com>
2023-12-29 12:31:27 +0300

    Habit options are added to the habit options screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -15,4 +15,3 @@ const AppNavigator = () => {
   };
   
   export default AppNavigator;
-  
--- src/screens/Habitoptions.js
+++ src/screens/Habitoptions.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+const options = [
+  { id: 1, name: "Exercise" },
+  { id: 2, name: "Read a Book" },
+  { id: 3, name: "Drink Water" },
+];
 
 const Habitoptions = () => {
+  const handleSelectOption = (optionName) => {
+    console.log(`Selected option: ${optionName}`); 
+  };
+  const renderOptionItem = ({ item }) => (
+    <TouchableOpacity 
+      style={styles.optionItem} 
+      onPress={() => handleSelectOption(item.name)}
+    >
+      <Text style={styles.optionText}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Welcome to Habit Options!</Text>
+      <Text style={styles.header}>Welcome to Habit Options!</Text>
+      <FlatList
+        data={options}
+        renderItem={renderOptionItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 };
@@ -12,8 +35,22 @@ const Habitoptions = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  optionItem: {
+    padding: 15,
+    marginVertical: 5,
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+  },
+  optionText: {
+    fontSize: 18,
   },
 });
 
