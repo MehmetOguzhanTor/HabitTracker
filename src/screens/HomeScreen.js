5e9c2dbffa1a1a5644cb45a69ddc4761aef38cf0
MehmetOguzhanTor <mehmetoguzhantor@gmail.com>
2023-12-29 13:52:54 +0300

    The selected habit is passed to the home screen.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Button, Text, StyleSheet } from 'react-native';
 import ButtonComponent  from '../components/Button';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({ navigation, route }) => {
+  const [selectedHabit, setSelectedHabit] = useState(null);
+
+  useEffect(() => {
+    if (route.params?.selectedHabit) {
+      setSelectedHabit(route.params.selectedHabit);
+    }
+  }, [route.params?.selectedHabit]);
+
   return (
     <View style={styles.container}>
       <ButtonComponent  
         title="Go to Habit options" 
         onPress={() => navigation.navigate('Habitoptions')} 
       />
+      {selectedHabit && (
+        <Text style={styles.selectedHabitText}>
+          Selected Habit: {route.params?.selectedHabit}
+        </Text>
+      )}
     </View>
   );
 };
@@ -18,6 +31,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  selectedHabitText: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'red',
   },
 });
 
